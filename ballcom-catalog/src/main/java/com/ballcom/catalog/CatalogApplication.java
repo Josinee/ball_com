8e71f882cf39db26d76fb217f3869dccb57308fa
FleurH5 <fleurmaxyara@gmail.com>
2026-07-03 11:46:58 +0200

    file structure added

---
+++ ballcom-catalog/src/main/java/com/ballcom/catalog/CatalogApplication.java
@@ -0,0 +1,16 @@
+package com.ballcom.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@ComponentScan(basePackages = {"com.ballcom.shared", "com.ballcom.catalog"})
+public class CatalogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogApplication.class, args);
+    }
+    
+}
