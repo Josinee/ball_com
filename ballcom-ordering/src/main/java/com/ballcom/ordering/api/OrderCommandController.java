8b3a102c5024278ca8fdb79029b7fb55a8425e49
FleurH5 <fleurmaxyara@gmail.com>
2026-07-08 16:58:49 +0200

    link gelegd nu nog testen

--- ballcom-ordering/src/main/java/com/ballcom/ordering/api/OrderCommandController.java
+++ ballcom-ordering/src/main/java/com/ballcom/ordering/api/OrderCommandController.java
@@ -42,7 +42,7 @@ public class OrderCommandController {
 
             return ResponseEntity.accepted().location(URI.create("/orders/" + orderId)).body(new OrderAcceptedResponse(orderId));
 
-        } catch (IllegalStateException e) {
+        } catch (IllegalStateException | IllegalArgumentException e) {
 
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
         } catch (Exception e) {
